868ac8259de39cf7f0d2a056ce9629f26d58c11d
Fergusoncalv <fergusoncalv@hotmail.com>
2016-10-13 14:02:48 -0400

    updated fakedata

--- DFF/DFF/Models/Donation.cs
+++ DFF/DFF/Models/Donation.cs
@@ -7,9 +7,11 @@ namespace DFF.Models
 {
     public class Donation
     {
+        public int ID { get; set; }
+        public int DonorId { get; set; }
         public DateTime DateAndTime { get; set; }
         public string Location { get; set; }
         public string TyofIdFood { get; set; }
-        public string pictureUrl { get; set; }
+        //public string pictureUrl { get; set; }
     }
 }
--- DFF/DFF/Models/RecieverDonor.cs
+++ DFF/DFF/Models/RecieverDonor.cs
@@ -5,10 +5,11 @@ using System.Web;
 
 namespace DFF.Models
 {
-    public class RecieverDoner { 
+    public class RecieverDonor { 
     
         public Login loginInfor { get; set; }
         public bool IsDoner { get; set; }
+        public bool IsReciever { get; set; }
         List<Donation> donations { get; set; }
     }
 }
