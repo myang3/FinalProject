6c52cb68419103de7871d4e203431182e3990bbb
moyang9192 <moyang9192@gmail.com>
2016-10-13 01:49:59 -0400

    Updated MVC added Landing Page etc.
    Use this rather than FinalProjectDFF

---
+++ DFF/DFF/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DFF.Models;
+
+namespace DFF.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult RegisterEvent()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult RegisterEvent(RegisterEventModel form)
+        {
+            if (ModelState.IsValid)
+            {
+                return View();
+
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+    }
+}
---
+++ DFF/DFF/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DFF.Startup))]
+namespace DFF
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
