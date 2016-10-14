14763c52603803c55dd263d9b8c5aed6ec693133
regdavco <regdavco1@hotmail.com>
2016-10-14 09:13:49 -0400

    new branch of dff with listing

--- DFF/DFF/Controllers/HomeController.cs
+++ DFF/DFF/Controllers/HomeController.cs
@@ -13,7 +13,6 @@ namespace DFF.Controllers
         {
             return View();
         }
-
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
---
+++ DFF/DFF/Controllers/ListingController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DFF.Controllers
+{
+    public class ListingController : Controller
+    {
+        List<Donor> donorList = new List<Donors>() {
+                    new Donor(){ DonorID=1, DonorName="Steve", Donation = "50 pita wraps"},
+                    new Donor(){ DonorID=2, DonorName="Bill", Donation= "5 cheese pizzas" },
+                    new Donor(){ DonorID=3, DonorName="Ram", Donation= "salad and dessing" },
+                    new Donor(){ DonorID=4, DonorName="Ron", Donation= "mixed sandwich trays" },
+                    new Donor(){ DonorID=5, DonorName="Rob", Donation= "Pork chop, mashed potatoes,gravy for 27" }
+                    };
+
+
+        // GET: Listing
+        public ViewResult Index()
+        {
+            ViewBag.Donor();
+            return View();
+        }
+    }
+}
