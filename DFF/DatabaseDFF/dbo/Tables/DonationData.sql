acc09af7a8cc749bcc42118fa119d65a6bea02c6
moyang9192 <moyang9192@gmail.com>
2016-10-14 12:44:04 -0400

    added databaseproject

---
+++ DFF/DatabaseDFF/dbo/Tables/DonationData.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[DonationData] (
+    [DonationID] INT            IDENTITY (1, 1) NOT NULL,
+    [DonorID]    INT            NOT NULL,
+    [FoodType]   NVARCHAR (MAX) NOT NULL,
+    [Location]   NVARCHAR (MAX) NOT NULL,
+    [PickupTime] DATETIME       NOT NULL,
+    [Picture]    IMAGE          NULL,
+    CONSTRAINT [PK_DonationData] PRIMARY KEY CLUSTERED ([DonationID] ASC),
+    CONSTRAINT [FK_DonationData_DonorData] FOREIGN KEY ([DonorID]) REFERENCES [dbo].[DonorData] ([DonorID])
+);
+
