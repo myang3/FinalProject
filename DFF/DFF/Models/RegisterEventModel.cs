5f07e677996950d83ffa6be70612dee2844207fe
Fergusoncalv <fergusoncalv@hotmail.com>
2016-10-14 09:27:43 -0400

    edited registereventmodel changed int to datetime

--- DFF/DFF/Models/RegisterEventModel.cs
+++ DFF/DFF/Models/RegisterEventModel.cs
@@ -11,8 +11,8 @@ namespace DFF.Models
         [StringLength(20), RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "Use letters only please")]
         [Required(ErrorMessage = "Please enter your name")]
         public string Name { get; set; }
-        public int Time { get; set; }
-        public int Date { get; set; }
+        public DateTime Time { get; set; }
+        public DateTime Date { get; set; }
         public string Email { get; set; }
         public int Phone { get; set; }
         public bool? NeedAssist { get; set; }
