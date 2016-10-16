61f307549b80412547676386bd449582beb1d048
Darius Dee <dhunter18@hawkmail.hfcc.edu>
2016-10-15 23:07:27 -0400

    Switched Database data types from date and time to nvarchar

--- FoodFinder/DatabaseDFF/dbo/Tables/DonationData.sql
+++ FoodFinder/DatabaseDFF/dbo/Tables/DonationData.sql
@@ -3,10 +3,10 @@
     [Name]       NVARCHAR (MAX) NOT NULL,
     [Email]      NVARCHAR (MAX) NOT NULL,
     [Phone]      NVARCHAR (15)  NOT NULL,
-    [FoodType]   NVARCHAR (MAX) NOT NULL,
     [Location]   NVARCHAR (MAX) NOT NULL,
-    [PickupDate] DATE           NOT NULL,
-    [PickupTime] TIME (7)       NOT NULL,
+    [FoodType]   NVARCHAR (MAX) NOT NULL,
+    [PickupDate] NVARCHAR(15)           NOT NULL,
+    [PickupTime] NVARCHAR(15)       NOT NULL,
     CONSTRAINT [PK_Donation] PRIMARY KEY CLUSTERED ([DonationID] ASC)
 );
 
