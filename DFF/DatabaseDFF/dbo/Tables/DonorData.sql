6e7e87a5898b7ba17951d8fb18d8858271e60393
moyang9192 <moyang9192@gmail.com>
2016-10-16 13:15:30 -0400

    Deleted Old Files
    To avoid confusion

--- DFF/DatabaseDFF/dbo/Tables/DonorData.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[DonorData] (
-    [DonorID] INT            IDENTITY (1, 1) NOT NULL,
-    [Name]    NVARCHAR (MAX) NOT NULL,
-    [Email]   NVARCHAR (MAX) NOT NULL,
-    [Phone]   NVARCHAR (15)  NOT NULL,
-    CONSTRAINT [PK_UserData_1] PRIMARY KEY CLUSTERED ([DonorID] ASC)
-);
-
--- DFFProject/DatabaseDFF/dbo/Tables/MatchUp.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[MatchUp] (
-    [MatchID]    INT IDENTITY (1, 1) NOT NULL,
-    [DonationID] INT NOT NULL,
-    [ReceiverID] INT NOT NULL,
-    CONSTRAINT [PK_MatchUp] PRIMARY KEY CLUSTERED ([MatchID] ASC),
-    CONSTRAINT [FK_MatchUp_DonationData] FOREIGN KEY ([DonationID]) REFERENCES [dbo].[DonationData] ([DonationID]),
-    CONSTRAINT [FK_MatchUp_ReceiverData] FOREIGN KEY ([ReceiverID]) REFERENCES [dbo].[ReceiverData] ([ReceiverID])
-);
-
