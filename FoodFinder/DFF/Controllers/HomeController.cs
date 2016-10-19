100de7c35e001d6c32765683be110657d5a28ffa
myang3 <moyang9192@hotmail.com>
2016-10-19 10:32:19 -0400

    Working on Merging Darius's Codes, incomplete

--- FoodFinder/DFF/Controllers/HomeController.cs
+++ FoodFinder/DFF/Controllers/HomeController.cs
@@ -4,6 +4,12 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using DFF.Models;
+using System.Data.Entity;
+using RestSharp;
+using RestSharp.Authenticators;
+using System.Threading.Tasks;
+using System.Net.Mail;
+using System.Net;
 
 namespace DFF.Controllers
 {
@@ -29,5 +35,51 @@ namespace DFF.Controllers
             return View();
         }
 
-    }
+
+        //--------------Email using SMTP client and gmail-----------------------------------
+        //-------------- must use a post method to send---------
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Contact(EmailFormModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var body = "<p>Email From: {0} ({1})</p><p>Message:</p><p>{2}</p>";
+                var message = new MailMessage();
+
+                message.To.Add(new MailAddress("dariushunter777@gmail.com"));  // replace with valid value 
+                message.To.Add(new MailAddress("darius777hunter@gmail.com"));  // replace with valid value 
+                message.To.Add(new MailAddress("dragonpride777@gmail.com"));  // replace with valid value 
+                message.From = new MailAddress("DetroitFoodFinders@DFF.com");  // replace with valid value
+                message.Subject = "It works";
+                message.Body = "We win";
+                message.IsBodyHtml = true;
+
+                using (var smtp = new SmtpClient())
+                {
+                    var credential = new NetworkCredential
+                    {
+                       UserName = "detroitfoodfinders@gmail.com",  // replace with valid value
+                       Password = "foodfinder1234"  // replace with valid value
+                    };
+                    smtp.Credentials = credential;
+                    smtp.Host = "smtp.gmail.com";
+                    smtp.Port = 587;
+                    smtp.EnableSsl = true;
+                    await smtp.SendMailAsync(message);
+                    return RedirectToAction("Sent");
+                }
+            }
+            return View(model);
+        }
+        public ActionResult Sent()
+        {
+            ViewBag.Message = "Email works. Fix and modify now";
+           
+                       return View();
+        }
+  
+ 
+      }
 }
